7ad3015722f0cc603adff277f6ed160d2fb86caf
Haftom <haftish.tsegay@gmail.com>
2025-11-08 20:19:31 -0100

    feat: made Slot concise with midnight crossing intervals

--- Core/Slot.cs
+++ Core/Slot.cs
@@ -1,33 +1,22 @@
 namespace Core;
 
-public sealed class Slot(TimeOnly? start = null, TimeOnly? end = null) : IEquatable<Slot> {
-    public TimeOnly Start { get; } = start ?? TimeOnly.MinValue;
-    public TimeOnly End { get; } = end ?? TimeOnly.MaxValue;
-    public TimeSpan Span => End - Start;
-    public bool IsPositive =>  Start < End;
-    public bool IsFullDay => Start == TimeOnly.MinValue && End == TimeOnly.MaxValue;
+public record Slot(TimeOnly Start, TimeSpan Span) {
+    public TimeOnly End => Start.Add(Span);
+    public bool IsPositive =>  Span > TimeSpan.Zero;
+    public bool IsFullDay => Start == TimeOnly.MinValue && Start == End;
+    private long EndInTicks => Start.Ticks + Span.Ticks;
     
-    public Slot(TimeOnly start, TimeSpan span) : this(start, start.Add(span)){}
+    public Slot(TimeOnly? start = null, TimeSpan? span = null)
+        : this(start ?? TimeOnly.MinValue, 
+            span ?? TimeSpan.FromTicks(TimeOnly.MaxValue.Ticks 
+                - (start?.Ticks ?? TimeOnly.MinValue.Ticks) + 1)) {}
+    public static Slot FullDay => new(TimeOnly.MinValue, TimeSpan.FromHours(24));
+    public static Slot UntilMidnight(TimeOnly start) => new(start, TimeSpan.FromTicks(TimeOnly.MaxValue.Ticks - start.Ticks + 1));
+    public static Slot FromMidnight(TimeSpan span) => new(TimeOnly.MinValue, span);
 
-    public bool Equals(Slot? other) {
-        if (other is null) return false;
-        if (ReferenceEquals(this, other)) return true;
-        return Start.Equals(other.Start) && End.Equals(other.End);
+    public Slot Overlap(Slot other) {
+        return new Slot(Start > other.Start ? Start : other.Start, TimeSpan.FromTicks(
+            Math.Min(EndInTicks, other.EndInTicks) 
+            - Math.Max(Start.Ticks, other.Start.Ticks)));
     }
-
-    public override bool Equals(object? obj) {
-        return ReferenceEquals(this, obj) || obj is Slot other && Equals(other);
-    }
-
-    public override int GetHashCode() {
-        return HashCode.Combine(Start, End);
-    }
-
-    public static bool operator ==(Slot? left, Slot? right) {
-        return Equals(left, right);
-    }
-
-    public static bool operator !=(Slot? left, Slot? right) {
-        return !Equals(left, right);
-    }
-} 
+}
